f09b9e43dbcce7a83dcdc6fb0e006f2cf6854bd8
Nick Leso <nickleso.work@gmail.com>
2023-12-15 11:33:40 +0200

    add main page markup

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,45 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  void onQiuzStart() {
+    print("**start quiz**");
+  }
+
   @override
   Widget build(context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Quiz App"),
+        body: Container(
+          decoration: const BoxDecoration(color: Colors.amber),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 250,
+                ),
+                const SizedBox(height: 36),
+                const Text(
+                  "Learn Flutter the fun way!",
+                  style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                ),
+                const SizedBox(height: 24),
+                FilledButton(
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Colors.amber[700],
+                  ),
+                  onPressed: onQiuzStart,
+                  child: const Text(
+                    "Start Quiz",
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 0, 0, 0), fontSize: 16),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
